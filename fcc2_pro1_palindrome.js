6b8badb1733b64369c79c53555148512fb65259e
Catherine <codingk8@gmail.com>
2018-12-15 21:15:55 +0100

    Update fcc2_pro1_palindrome.js

--- fcc2_pro1_palindrome.js
+++ fcc2_pro1_palindrome.js
@@ -1,5 +1,34 @@
 /* xxx */
 
-// My solution
+// My 1st solution
+
+function palindrome(str) {
+    let regex = /[^a-z0-9]/g;
+ 
+    let cleanString = str.toLowerCase().replace(regex, "");
+ console.log(cleanString);
+ 
+    let array = cleanString.split("");
+ console.log(array);
+ 
+    let array2 = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        array2.push(array[i]);
+        
+    }
+console.log(array2);
+
+    let joinString = array2.join("");
+console.log(joinString);
+    
+    return joinString === cleanString;
+}
 
 // My favorite solution
+
+
+// Other solutions
+
+
+
+/* That's all folks! */

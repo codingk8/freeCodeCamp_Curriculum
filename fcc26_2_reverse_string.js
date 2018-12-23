1af94689aa80d0c11965ebfc053d96bcfa5c776e
Catherine <codingk8@gmail.com>
2018-12-23 19:52:14 +0100

    Update fcc26_2_reverse_string.js

--- fcc26_2_reverse_string.js
+++ fcc26_2_reverse_string.js
@@ -1,1 +1,8 @@
-/* xxx */
+/* FCC26 Basic Algorithm Scripting - Reverse a string */
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+reverseString("hello");
+
+// That's it :-)

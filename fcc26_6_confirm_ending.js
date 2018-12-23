5a771e8b348253ee4e56339d8147916f715826da
Catherine <codingk8@gmail.com>
2018-12-23 20:07:42 +0100

    Update fcc26_6_confirm_ending.js

--- fcc26_6_confirm_ending.js
+++ fcc26_6_confirm_ending.js
@@ -1,1 +1,17 @@
-/* xxx */
+/* FCC26 Basic Algorithm Scripting - Confirm the Ending */
+
+function confirmEnding(str, target) {
+  return str.slice(str.length - target.length) === target;
+}
+confirmEnding("He has to give me a new name", "name");
+
+// Possible solution with Regexp
+function confirmEnding(str, target) {
+  let newTarget = target.concat("$");
+  let myRegEx = new RegExp(newTarget, 'i');
+  return myRegEx.test(str);
+}
+
+// PR the regexp solution
+
+// The end ;.)

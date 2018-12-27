715e2ebcd847695ae3acd7a5f14cb021a0c77437
Catherine <codingk8@gmail.com>
2018-12-27 20:07:44 +0100

    Update fcc2_pro2_roman_converter.js

--- fcc2_pro2_roman_converter.js
+++ fcc2_pro2_roman_converter.js
@@ -1,1 +1,60 @@
-/* xxx */
+/* Free Code Camp - Part 2: Basic JavaScript - Project 2: Roman Numeral Converter */
+
+// 1st solution
+
+function convertToRoman(num) {
+    let numTable = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    let romanNumber = "";
+    for ( let i in numTable) {
+        while (num >= numTable[i]) {
+        romanNumber += i;
+        num -= numTable[i];
+       }
+    }
+ return romanNumber;
+}
+convertToRoman(36);
+
+// 2nd solution
+
+function convertToRoman(num) {
+    let numTable = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    let romanNumber = "";
+    for ( let i in numTable) {
+        while (num >= numTable[i]) {
+        romanNumber += i;
+        num -= numTable[i];
+       }
+    }
+ return romanNumber;
+}
+
+convertToRoman(36);
